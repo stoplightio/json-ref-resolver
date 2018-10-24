f5fd86190515a90d7ebb77cd9de0e0e3d547276a
Marc MacLeod <marbemac@gmail.com>
2018-10-23 21:55:49 -0500

    refactor: add project scaffold

---
+++ rollup.config.ts
@@ -0,0 +1,45 @@
+import camelCase from 'lodash.camelcase';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+
+const pkg = require('./package.json');
+
+const libraryName = 'json-ref-resolver';
+
+export default {
+  input: `src/${libraryName}.ts`,
+
+  output: [
+    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true },
+  ],
+
+  // Indicate here external modules you don't want to include in your bundle (i.e.: 'lodash')
+  external: [],
+
+  watch: {
+    include: 'src/**',
+  },
+
+  plugins: [
+    // Allow json resolution
+    json(),
+
+    // Compile TypeScript files
+    typescript({ useTsconfigDeclarationDir: true }),
+
+    // Bundle cjs modules
+    commonjs(),
+
+    // Allow node_modules resolution, so you can use 'external' to control
+    // which external modules to include in the bundle
+    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    resolve(),
+
+    // Resolve source maps to the original source
+    sourceMaps(),
+  ],
+};
