e15f2e1308fe5edc6fee82a0927e28eeeda612d0
Marc MacLeod <marbemac@gmail.com>
2018-10-26 20:17:17 -0500

    fix(npm): include readme and other files in dist

--- scripts/post-build-preparation.ts
+++ scripts/post-build-preparation.ts
@@ -1,5 +1,5 @@
 const { echo } = require('shelljs');
-const { readFileSync, writeFileSync } = require('fs');
+const { readFileSync, writeFileSync, copyFileSync } = require('fs');
 const _pick = require('lodash/pick');
 
 const pkg = JSON.parse(readFileSync('package.json') as any);
@@ -19,9 +19,11 @@ const releasePkg = _pick(pkg, [
   'dependencies',
 ]);
 
-echo('Copying package.json file to dist folder.');
+echo('Copying extra files to dist folder.');
 writeFileSync('dist/package.json', JSON.stringify(releasePkg, null, 2));
-echo('Package.json file copied.');
+copyFileSync('README.md', 'dist/README.md');
+copyFileSync('LICENSE', 'dist/LICENSE');
+echo('Files copied.');
 
 // TS needs to know that this is not beling loaded globally
 // https://stackoverflow.com/a/41975448/197017
